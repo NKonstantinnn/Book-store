b29c5d9907c292f9c76f985c992f202fa4f3b7fa
kostya007 <kostya.1998.nosarev@mail.ru>
2019-07-19 17:37:05 +0700

    Connect react and redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import BookStoreService from './services/bookstore-service';
+import { BookstoreServiceProvider } from './components/bookstore-service-context/bookstore-service-context';
+import ErrorBoundry from './components/error-boundry/error-boundry';
+import App from './components/app/app';
+
+import store from './store';
+
+const bookstoreService = new BookStoreService();
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <ErrorBoundry>
+            <BookstoreServiceProvider value={ bookstoreService }>
+                <Router>
+                    <App />
+                </Router>
+            </BookstoreServiceProvider>
+        </ErrorBoundry>
+    </Provider>,
+    document.getElementById('root')
+);
