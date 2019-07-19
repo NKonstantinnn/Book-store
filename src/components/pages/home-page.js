23b7defee1de3a5fcfd10bd8acb77d23f7227e18
kostya007 <kostya.1998.nosarev@mail.ru>
2019-07-19 18:10:58 +0700

    Add BookList

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import BookList from '../book-list/book-list';
 
 const HomePage = () => {
-    return <div>Home Page</div>;
+    const books = [
+        {
+            id: 1,
+            title: 'Production-Ready Microservices',
+            author: 'Susan.J Fowler'
+        },
+        {
+            id: 2,
+            title: 'Release It',
+            author: 'Michael T. Nygard'
+        }
+    ]
+    return <div><BookList books={ books } /></div>;
 }
 
 export default HomePage;
