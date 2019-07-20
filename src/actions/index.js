4c5f88998074f29162e49e928952f9b4c3ead0eb
kostya007 <kostya.1998.nosarev@mail.ru>
2019-07-21 00:11:09 +0700

    Realize  a shopping cart table component functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,45 @@
 const booksLoaded = (newBooks) => {
-    return {
-      type: 'FETCH_BOOKS_SUCCESS',
-      payload: newBooks
-    };
+  return {
+    type: 'FETCH_BOOKS_SUCCESS',
+    payload: newBooks
   };
+};
   
-  const booksRequested = () => {
-    return {
-      type: 'FETCH_BOOKS_REQUEST'
-    };
+const booksRequested = () => {
+  return {
+    type: 'FETCH_BOOKS_REQUEST'
   };
+};
 
-  const booksError = (error) => {
-    return {    
-        type: 'FETCH_BOOKS_FAILURE',
-        payload: error
-    };
+const booksError = (error) => {
+  return {    
+      type: 'FETCH_BOOKS_FAILURE',
+      payload: error
   };
+};
+
+const bookAddedToCart = (bookId) => {
+  return {
+    type: 'BOOK_ADDED_TO_CART',
+    payload: bookId
+  };
+};
+
+const bookRemovedFromCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
+const allBookRemovedFromCart = (bookId) => {
+  return {
+    type: 'ALL_BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
+
 
 const fetchBooks = (bookstoreService, dispatch) => () => {
     dispatch(booksRequested());
@@ -26,5 +49,8 @@ const fetchBooks = (bookstoreService, dispatch) => () => {
 };
   
   export {
-    fetchBooks
+    fetchBooks,
+    bookAddedToCart,
+    bookRemovedFromCart,
+    allBookRemovedFromCart
   };
--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,21 +3,25 @@ import BookListItem from '../book-list-item/book-list-item';
 import { connect } from 'react-redux';
 
 import withBookstoreService from '../hoc/with-bookstore-service';
-import { fetchBooks } from '../../actions/index';
+import { fetchBooks, bookAddedToCart } from '../../actions/index';
 import compose from '../../utils/compose';
 
 import './book-list.css'
 import Spinner from '../spinner/spinner';
 import ErrorIndicator from '../error-indicator/error-indicator';
 
-const BookList = ({ books }) => {
+const BookList = ({ books, onAddedToCart }) => {
 
     return (
         <ul className="book-list">
             {
                 books.map((book) => {
                     return (
-                        <li key={ book.id }><BookListItem book={ book } /></li>
+                        <li key={ book.id }>
+                            <BookListItem 
+                                book={ book }
+                                onAddedToCart={ () => onAddedToCart(book.id) } />
+                        </li>
                     );
                 })
             }
@@ -32,7 +36,7 @@ class BookListContainer extends Component {
     }
 
     render() {
-        const { books, loading, error } = this.props;
+        const { books, loading, error, onAddedToCart } = this.props;
 
         if(loading) {
             return <Spinner />;
@@ -42,7 +46,7 @@ class BookListContainer extends Component {
             return <ErrorIndicator />
         }
 
-        return <BookList books={ books } />
+        return <BookList books={ books } onAddedToCart={ onAddedToCart } />
     }
 }
 
@@ -53,7 +57,8 @@ const mapStateToProps = ({ books, loading, error }) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
     const { bookstoreService } = ownProps;
     return {
-        fetchBooks: fetchBooks(bookstoreService, dispatch)
+        fetchBooks: fetchBooks(bookstoreService, dispatch),
+        onAddedToCart: (id) => dispatch(bookAddedToCart(id))
     };
 }
 
